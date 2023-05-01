f37563bcd69f16a18878d72ee985db7eb8989185
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 14:36:41 +0400

    Added @Autowired CustomerRepository into CustomerService

--- demo/src/main/java/com/example/demo/Services/CustomerService.java
+++ demo/src/main/java/com/example/demo/Services/CustomerService.java
@@ -1,7 +1,10 @@
 package com.example.demo.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+    @Autowired
+    CustomerService customerService;
 }
