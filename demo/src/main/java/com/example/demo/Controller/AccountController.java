7f85aa48011c1e5de66bfe8fd1c6e442da185ff8
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 16:35:29 +0400

    Added  @RequestMapping(value = AccountData ,method = RequestMethod.POST) to the method in AccountController

--- demo/src/main/java/com/example/demo/Controller/AccountController.java
+++ demo/src/main/java/com/example/demo/Controller/AccountController.java
@@ -1,10 +1,22 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping(value="Account")
 public class AccountController {
+    @Autowired
+    AccountService accountService;
+    @RequestMapping(value = "AccountData" ,method = RequestMethod.POST)
+    public void createAccountData(@RequestParam Integer accountNumber,Double balance ) throws ParseException {
+        accountService.createAccountData(accountNumber,balance);
+    }
 
 }
