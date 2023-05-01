d5b5bb15f92add457777cb848807f2d63d7b9c1a
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 11:37:41 +0400

    Added proporties into class Transaction in Models

--- demo/src/main/java/com/example/demo/Models/Transaction.java
+++ demo/src/main/java/com/example/demo/Models/Transaction.java
@@ -1,5 +1,6 @@
 package com.example.demo.Models;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,12 +8,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@Data
 public class Transaction extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer id;
+    Double amount;
+    Date transactionDate;
 }
