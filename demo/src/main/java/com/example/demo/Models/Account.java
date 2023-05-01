05dfd506c07accfa0489a4cbd4bafba22df4d6f6
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 11:15:46 +0400

    Added @Entity,@Getter and Setter for each classes in Models

--- demo/src/main/java/com/example/demo/Models/Account.java
+++ demo/src/main/java/com/example/demo/Models/Account.java
@@ -1,4 +1,12 @@
 package com.example.demo.Models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
 public class Account extends BaseEntity{
 }
