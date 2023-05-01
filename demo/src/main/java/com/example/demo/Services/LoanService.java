712e9af28a857a502e02cfef03b10dd55ce469f3
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 14:34:54 +0400

    Added @Autowired LoanRepository into LoanService

--- demo/src/main/java/com/example/demo/Services/LoanService.java
+++ demo/src/main/java/com/example/demo/Services/LoanService.java
@@ -1,7 +1,11 @@
 package com.example.demo.Services;
 
+import com.example.demo.Repositories.LoanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoanService {
+    @Autowired
+    LoanRepository loanRepository;
 }
