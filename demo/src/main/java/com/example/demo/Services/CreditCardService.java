cab776c2517a71650c6064e2002c38849cf9dcb6
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 14:39:01 +0400

    Added @Autowired CreditCardRepository into CreditCardService

--- demo/src/main/java/com/example/demo/Services/CreditCardService.java
+++ demo/src/main/java/com/example/demo/Services/CreditCardService.java
@@ -1,7 +1,11 @@
 package com.example.demo.Services;
 
+import com.example.demo.Repositories.CreditCardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CreditCardService {
+    @Autowired
+    CreditCardRepository creditCardRepository;
 }
--- demo/src/main/java/com/example/demo/Services/CustomerService.java
+++ demo/src/main/java/com/example/demo/Services/CustomerService.java
@@ -1,10 +1,11 @@
 package com.example.demo.Services;
 
+import com.example.demo.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
     @Autowired
-    CustomerService customerService;
+    CustomerRepository customerRepository;
 }
