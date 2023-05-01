385813ace64e362a6d100abd86efd44cef04efac
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 15:27:05 +0400

    Added value to table customer using set

--- demo/src/main/java/com/example/demo/Services/CustomerService.java
+++ demo/src/main/java/com/example/demo/Services/CustomerService.java
@@ -1,11 +1,22 @@
 package com.example.demo.Services;
 
+import com.example.demo.Models.Customer;
 import com.example.demo.Repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
+    public void  createCustomer(String fName,String lName ,String email,String phone) {
+        Customer customer=new Customer();
+        customer.setFirstName(fName);
+        customer.setLastName(lName);
+        customer.setEmail(email);
+        customer.setPhone(phone);
+        customerRepository.save(customer);
+    }
 }
