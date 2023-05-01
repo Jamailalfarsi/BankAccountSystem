e34320b5941de40e0dd0cce15025ce77cf654428
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-01 12:01:08 +0400

    Added @OneToMany,@JoinColumn to defining the foreign key which is ID in Customer class

--- demo/src/main/java/com/example/demo/Models/Customer.java
+++ demo/src/main/java/com/example/demo/Models/Customer.java
@@ -3,10 +3,7 @@ package com.example.demo.Models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -19,4 +16,8 @@ public class Customer extends BaseEntity{
     String lastName;
     String email;
     String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", referencedColumnName = "id")// defining the foreign key which is ID
+    Account account;
 }
