2f343a5759c15ce69666f91d69ab337688b440ba
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-02 11:32:20 +0400

    using set for createdDate, UpdatedDate and isActive

--- demo/src/main/java/com/example/demo/Services/CustomerService.java
+++ demo/src/main/java/com/example/demo/Services/CustomerService.java
@@ -11,12 +11,15 @@ import java.util.Date;
 public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
-    public void  createCustomer(String fName,String lName ,String email,String phone) {
+    public void  createCustomer(String fName,String lName ,String email,String phone,Date createdDate,Date updatedDate, Boolean isActive) {
         Customer customer=new Customer();
         customer.setFirstName(fName);
         customer.setLastName(lName);
         customer.setEmail(email);
         customer.setPhone(phone);
+        customer.setCreatedDate(createdDate);
+        customer.setUpdatedDate(updatedDate);
+        customer.setIsActive(isActive);
 
         customerRepository.save(customer);
     }
