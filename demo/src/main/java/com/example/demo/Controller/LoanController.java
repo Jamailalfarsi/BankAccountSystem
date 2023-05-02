69f907941bd8fbdb561c3cf6d74f7489f5224887
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-02 13:53:03 +0400

    Added createLoanTable method in loanController

--- demo/src/main/java/com/example/demo/Controller/LoanController.java
+++ demo/src/main/java/com/example/demo/Controller/LoanController.java
@@ -1,9 +1,25 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Models.Loan;
+import com.example.demo.Repositories.LoanRepository;
+import com.example.demo.Services.CustomerService;
+import com.example.demo.Services.LoanService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping(value="Loan")
 public class LoanController {
+
+        @Autowired
+        LoanService loanService;
+        @RequestMapping(value = "LoanTableData" ,method = RequestMethod.POST)
+        public void createLoanTable(@RequestParam Double amount,Double rate,String createdDate,String updatedDate, Boolean isActive) throws ParseException {
+            loanService.createLoanTable(amount, rate,createdDate,updatedDate,isActive);
+        }
 }
