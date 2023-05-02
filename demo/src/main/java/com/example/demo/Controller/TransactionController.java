6578198a5677b5671f6b426d299d6eec8cd3f9f2
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-02 14:15:08 +0400

    Created metod createTransactionTable() in TransactionController

--- demo/src/main/java/com/example/demo/Controller/TransactionController.java
+++ demo/src/main/java/com/example/demo/Controller/TransactionController.java
@@ -1,9 +1,22 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Services.LoanService;
+import com.example.demo.Services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping(value="Transaction")
 public class TransactionController {
+    @Autowired
+    TransactionService transactionService;
+    @RequestMapping(value = "TransactionTableData" ,method = RequestMethod.POST)
+    public void createTransactionTable(@RequestParam Double amount,String transactionDate ,String createdDate,String updatedDate, Boolean isActive) throws ParseException {
+        transactionService.createTransactionTable(amount, transactionDate,createdDate,updatedDate,isActive);
+    }
 }
