ed5dae5f4952fafa11485009a7be4581ecbac017
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-02 11:35:01 +0400

    Added the prameters in CustomerController (CreatedDate,UpdatedDate,isActive)

--- demo/src/main/java/com/example/demo/Controller/CustomerController.java
+++ demo/src/main/java/com/example/demo/Controller/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
+import java.util.Date;
 
 @RestController
 @RequestMapping(value="Customer")
@@ -17,8 +18,8 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
     @RequestMapping(value = "CustomerData" ,method = RequestMethod.POST)
-    public void createCustomer(@RequestParam String fName, String lName , String email, String phone) throws ParseException {
-        customerService.createCustomer(fName, lName, email, phone);
+    public void createCustomer(@RequestParam String fName, String lName , String email, String phone, Date createdDate, Date updatedDate, Boolean isActive) throws ParseException {
+        customerService.createCustomer(fName, lName, email, phone,createdDate,updatedDate,isActive);
     }
 
 
