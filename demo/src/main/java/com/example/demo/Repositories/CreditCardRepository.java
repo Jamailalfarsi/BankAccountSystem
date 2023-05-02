d69a9aa38fbd4e9dfde153caf552e47a5c7908ad
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-02 15:16:30 +0400

    Created Query in CreditCardRepository for getAllCreditCards

--- demo/src/main/java/com/example/demo/Repositories/CreditCardRepository.java
+++ demo/src/main/java/com/example/demo/Repositories/CreditCardRepository.java
@@ -2,9 +2,14 @@ package com.example.demo.Repositories;
 
 import com.example.demo.Models.Account;
 import com.example.demo.Models.CreditCard;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CreditCardRepository extends CrudRepository<CreditCard,Integer> {
+    @Query(value = "SELECT s from CreditCard s")
+    List<CreditCard> getAllCreditCards();
 }
