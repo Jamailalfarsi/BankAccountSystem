aad8a22784a3fd6807b65a7860feb7d47ddbc75d
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-02 15:27:03 +0400

    Created a Query for getAllLoans() into LoanRepository

--- demo/src/main/java/com/example/demo/Repositories/LoanRepository.java
+++ demo/src/main/java/com/example/demo/Repositories/LoanRepository.java
@@ -2,9 +2,14 @@ package com.example.demo.Repositories;
 
 import com.example.demo.Models.Customer;
 import com.example.demo.Models.Loan;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LoanRepository extends CrudRepository<Loan,Integer> {
+    @Query(value="SELECT s from Loan s")
+    List<Loan> getAllLoans();
 }
--- demo/src/main/java/com/example/demo/Repositories/TransactionRepository.java
+++ demo/src/main/java/com/example/demo/Repositories/TransactionRepository.java
@@ -1,11 +1,17 @@
 package com.example.demo.Repositories;
 
+import com.example.demo.Models.Customer;
 import com.example.demo.Models.Loan;
 import com.example.demo.Models.Transaction;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction,Integer> {
+//    @Query(value="SELECT s from Transaction s")
+//    List<Transaction> getAllTransactions();
 
 }
