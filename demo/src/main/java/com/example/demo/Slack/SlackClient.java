c7f582f4d5314164acc5f0156f445ba3d0e8ce26
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-14 12:35:23 +0400

    Added @Component  annotation in slackClient

--- demo/src/main/java/com/example/demo/Slack/SlackClient.java
+++ demo/src/main/java/com/example/demo/Slack/SlackClient.java
@@ -1,4 +1,10 @@
 package com.example.demo.Slack;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SlackClient {
+    public String sendMessage(String text) {
+        return "Done";
+    }
 }
