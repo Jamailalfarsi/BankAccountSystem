aadfeee56c0d139554878c89269dc40932905ce0
Your NameJamailalfarsi <jamailalfarsi95@gmail.com>
2023-05-14 12:12:02 +0400

    Added code for security to user and admin in GeneralController

--- demo/src/main/java/com/example/demo/Controller/GeneralController.java
+++ demo/src/main/java/com/example/demo/Controller/GeneralController.java
@@ -1,8 +1,32 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Slack.SlackClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GeneralController {
+    @Autowired
+    SlackClient slackClient;
+
+    @GetMapping(value = "test")
+    @PreAuthorize("hasRole('USER')")
+    public String test(){
+        return "${spring.profiles.active}";
+    }
+
+//    @GetMapping(value = "slackMessage")
+//    public void message(@RequestParam String text){
+//        slackClient.sendMessage(text);
+//    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminOnly() {
+        return "You are an admin!";
+    }
 
 }
